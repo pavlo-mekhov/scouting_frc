1569e90ef7384a0b3212132da53f9486b4beb28c
Pavlo Mekhov <pavlo.mekhov@gmail.com>
2025-05-14 12:06:21 -0400

    Minor changes data management and local data

--- lib/match_scouting_page.dart
+++ lib/match_scouting_page.dart
@@ -1,6 +1,4 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class _CounterState extends State<Counter> {
 
